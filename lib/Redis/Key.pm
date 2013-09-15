cc7471fe0250385523cb66a616e1a3f25f166c8a
Ichinose Shogo <shogo82148@gmail.com>
2013-09-15 11:53:09 +0900

    initial commit

---
+++ lib/Redis/Key.pm
@@ -0,0 +1,35 @@
+package Redis::Key;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+use Redis;
+
+
+1;
+__END__
+
+=head1 NAME
+
+Redis::Key -
+
+=head1 SYNOPSIS
+
+  use Redis::Key;
+
+=head1 DESCRIPTION
+
+Redis::Key is
+
+=head1 AUTHOR
+
+Ichinose Shogo E<lt>shogo82148@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
