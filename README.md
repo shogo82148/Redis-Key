7db22a2d690a1bab9bfeeb9fe7b2bd751a8978b0
Ichinose Shogo <shogo82148@gmail.com>
2013-10-16 21:09:22 +0900

    README更新

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
-# NAME
-
-Redis::Key -
-
-# SYNOPSIS
-
-    use Redis::Key;
-
-# DESCRIPTION
-
-Redis::Key is
-
 # AUTHOR
 
 Ichinose Shogo <shogo82148@gmail.com>
+
+# SYNOPSIS
+
+    use Redis;
+    use Redis::Key;
+    my $redis = Redis->new;
+    
+
+    # basic usage
+    my $key = Redis::Key->new(redis => $redis, key => 'hoge');
+    $key->set('fuga');  # => $redis->set('hoge', 'fuga');
+    print $key->get;    # => $redis->get('hoge');
+    
+
+    # bind
+    my $key_unbound = Redis::Key->new(redis => $redis, key => 'hoge:{fugu}:piyo', need_bind => 1);
+    my $key_fugu = $key_unbound->bind(fugu => 'FUGU');
+    $key_fugu->set('foobar');      # => $redis->set('hoge:FUGU:piyo', 'foobar');
+    my @keys = $key_unbound->keys; # => $redis->keys('hoge:*:piyo');
+
+# DESCRIPTION
+
+Redis::Key is a wrapper class of Redis' keys.
 
 # SEE ALSO
 
--- lib/Redis/Key.pm
+++ lib/Redis/Key.pm
@@ -89,21 +89,30 @@ sub AUTOLOAD {
 1;
 __END__
 
-=head1 NAME
-
-Redis::Key -
-
-=head1 SYNOPSIS
-
-  use Redis::Key;
-
-=head1 DESCRIPTION
-
-Redis::Key is
-
 =head1 AUTHOR
 
 Ichinose Shogo E<lt>shogo82148@gmail.comE<gt>
+
+=head1 SYNOPSIS
+
+  use Redis;
+  use Redis::Key;
+  my $redis = Redis->new;
+  
+  # basic usage
+  my $key = Redis::Key->new(redis => $redis, key => 'hoge');
+  $key->set('fuga');  # => $redis->set('hoge', 'fuga');
+  print $key->get;    # => $redis->get('hoge');
+  
+  # bind
+  my $key_unbound = Redis::Key->new(redis => $redis, key => 'hoge:{fugu}:piyo', need_bind => 1);
+  my $key_fugu = $key_unbound->bind(fugu => 'FUGU');
+  $key_fugu->set('foobar');      # => $redis->set('hoge:FUGU:piyo', 'foobar');
+  my @keys = $key_unbound->keys; # => $redis->keys('hoge:*:piyo');
+
+=head1 DESCRIPTION
+
+Redis::Key is a wrapper class of Redis' keys.
 
 =head1 SEE ALSO
 
